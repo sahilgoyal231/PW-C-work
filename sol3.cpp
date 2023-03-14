8f51b942a09f542227d80e40085b1f4101bb34fd
sahilgoyal231 <116739740+sahilgoyal231@users.noreply.github.com>
2023-03-14 23:03:13 +0530

    Add files via upload

---
+++ sol3.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+using namespace std;
+int main(){
+
+    int length = 13;
+    int breadth = 11;
+    cout<<"Length : "<<length<<endl;
+    cout<<"Breadth :"<<breadth<<endl;
+    
+    int area = length*breadth;
+    cout<<"Area is : "<<area<<endl;
+
+    return 0 ;
+}
