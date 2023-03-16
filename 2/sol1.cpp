251095121cacaa17adea90861e5cebb0507c8966
sahilgoyal231 <116739740+sahilgoyal231@users.noreply.github.com>
2023-03-16 20:47:44 +0530

    Add files via upload

---
+++ 2/sol1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+    cout<<" First number : ";
+    cin>>a;
+
+    cout<<" Second number : ";
+    cin>>b;
+
+    cout<< (a==b) ? 1:0;
+
+    return 0;
+}
